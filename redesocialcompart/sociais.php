3a13845804567ab091ab4ce058db403f90b02f1f
RobertoJunior17 <104389402+RobertoJunior17@users.noreply.github.com>
2023-11-10 23:05:10 -0300

    Add files via upload

---
+++ redesocialcompart/sociais.php
@@ -0,0 +1,34 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Compartilhar em Redes Sociais</title>
+    <!-- Adicione os links para os ícones das redes sociais lembrar pra falar com Rodrigo -->
+    <!-- Certificar de incluir os ícones corretos para Facebook, Twitter e LinkedIn, no puede esquecer -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <style>
+        /* Estilos opcionais para os ícones */
+        .social-icons {
+            font-size: 24px;
+            margin: 5px;
+        }
+    </style>
+</head>
+<body>
+    <div class="social-icons">
+        <!-- Substituir as URLs com os links das noticias(o site que pq fica melhor), para poder compartilhar  -->
+        <a href="https://www.facebook.com/sharer/sharer.php?u=URL_DO_SEU_ARTIGO" target="_blank">
+            <i class="fab fa-facebook"></i>
+        </a>
+        <a href="https://twitter.com/intent/tweet?url=URL_DO_SEU_ARTIGO" target="_blank">
+            <i class="fab fa-twitter"></i>
+        </a>
+        <a href="https://www.linkedin.com/shareArticle?url=URL_DO_SEU_ARTIGO" target="_blank">
+            <i class="fab fa-linkedin"></i>
+        </a>
+    </div>
+</body>
+</html>
